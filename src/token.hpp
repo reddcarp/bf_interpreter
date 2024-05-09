6667d8373491dac776630b0a9f2340651f02e7db
Marvin Libam <libam.marvin@gmail.com>
2024-05-09 03:41:26 +0200

    Base: comments added

--- src/token.hpp
+++ src/token.hpp
@@ -9,26 +9,64 @@ namespace bf_interpreter {
 
 // Abstract classes
 
+/**
+ * @brief Represents a token in the BF interpreter.
+ */
 class Token {
  public:
+    /**
+     * @brief Constructs a new Token object with the specified character.
+     * 
+     * @param t The character representing the token.
+     */
     explicit Token(char t);
+
     virtual ~Token() = default;
 
+    /**
+     * @brief Performs the action associated with the token.
+     * 
+     * @param data_index The current data index.
+     * @param data The data vector.
+     * @return A pointer to the next token to be executed.
+     */
     virtual Token* action(size_t &data_index, std::vector<char> &data) = 0;
 
-    std::unique_ptr<Token> next_token = nullptr;
-    size_t repeated = 1;
-    const char token;
+    std::unique_ptr<Token> next_token = nullptr; /* A pointer to the next token. */
+    size_t repeated = 1; /* The number of times the token's action should be repeated. */
+    const char token; /* The character representing the token. */
 };
 
+/**
+ * @brief Represents a jump token in the BF interpreter.
+ */
 class JmpToken : public Token {
  public:
+    /**
+     * @brief Constructs a JmpToken object with the specified token character.
+     * 
+     * @param t The token character.
+     */
     explicit JmpToken(char t);
 
+    /**
+     * @brief Returns the next token to execute based on the jump condition.
+     * 
+     * @param data_index The current data index.
+     * @param data The data vector.
+     * @return A pointer to the next token to execute.
+     */
     Token* action(size_t &data_index, std::vector<char> &data) final;
+
+    /**
+     * @brief Checks the jump condition for the token.
+     *
+     * @param value The value at the current data index.
+     * @return bool True if the jump condition is met, false otherwise.
+     */
     virtual bool jmpCondition(int value) = 0;
 
-    Token *jmp_to;
+    Token *jmp_to; /* The token to jump to. */
 };
 // ==============================================================
 
--- src/tokenize.hpp
+++ src/tokenize.hpp
@@ -7,6 +7,12 @@
 
 namespace bf_interpreter {
 
+/**
+ * Tokenizes the input stream and returns a unique pointer to a Token object.
+ *
+ * @param stream The input stream to tokenize.
+ * @return A unique pointer to a Token object representing the tokenized stream.
+ */
 std::unique_ptr<Token> tokenizeStream(std::istream &stream);
 
 } // bf_interpreter
